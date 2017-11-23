abd8e9d88385b0c1f6701bfaaa22ba7217a85dd5
nateshmbhat <nateshmbhat1@gmail.com>
2017-11-23 17:07:15 +0530

    Torrent object now working

--- yifytops.py
+++ yifytops.py
@@ -1,4 +1,5 @@
 import os
+import json
 import urllib3
 import time;
 from bs4 import BeautifulSoup
@@ -20,7 +21,7 @@ class yify():
             
 
         def getinfo(self , quality='All' , minimum_rating = 0 , 
-        query_term = '' , genre='All' , sort_by='date_added' ,
+        query_term = '' , genre='' , sort_by='date_added' ,
         order_by = 'desc' , with_rt_ratings='false'):
             '''
             Gets all the info about the torrent and returns a dictionary containing all the info
@@ -33,12 +34,38 @@ class yify():
             url = url+ '?' +  urllib3.request.urlencode({
                 'quality' : quality , 
                 'minimum_rating' : minimum_rating , 
-                'query_term' : query_term , 
+                'query_term' : self.name, 
                 'genre' : genre , 
                 'sort_by' : sort_by , 
                 'order_by' : order_by , 
                 'with_rt_ratings' : with_rt_ratings
             })            
+
+            resp = get(url , timeout = 3) ;
+            data = json.loads(resp.text) ; 
+            movie = data["movies"][0] ;
+
+            self.id = movie.get('id')
+            self.url = movie.get('url')
+            self.imdb_code = movie.get('imdb_code')
+            self.title = movie.get('title')
+            self.title_long = movie.get('title_long')
+            self.slug  = movie.get('slug')
+            self.year = movie.get('year')
+            self.rating = movie.get('rating')
+            self.runtime = movie.get('runtime')
+            self.genres = movie.get('genres') 
+            self.summary = movie.get('summary')
+            self.description = movie.get('description') 
+            self.language = movie.get('language')
+            self.mpa_rating = movie.get('mpa_rating')
+            self.image_links = [movie.get('background_image'),
+            movie.get('background_image_original'), 
+            movie.get('small_cover_image'),
+            movie.get('medium_cover_image'),
+            movie.get('large_cover_image')]
+            
+
 
             print(url) ;
 
@@ -69,7 +96,6 @@ class yify():
     #             print(
     #                 "\nServer refused connection . \nSleeping for 5 seconds .....\nZZZzzzzzzzzz\n\n");
     #             time.sleep(5);
-
     #     self.soup = BeautifulSoup(resp.text, 'html.parser');
 
 
@@ -77,16 +103,16 @@ class yify():
     def get_top_seeded_torrents(self):
         '''Returns a list of Top Seeded Torrents which are listed in the Yify Website when the function is called '''
         soup = BeautifulSoup(get('https://www.yify-torrent.org/', timeout=3).text , 'html.parser') ;
-        self.topseeds = soup.find(id="topseed").find_all('a');
+        topseeds = soup.find(id="topseed").find_all('a');
 
 
         # topseeds_names =  [];
         # topseeds_links = [] ; 
         top_torrents = [] 
 
-        for i in self.topseeds:
+        for i in topseeds:
             movie = self.torrent(name=i.text , page = i.get('href')) ; 
-            movie.getinfo()
+            # movie.getinfo()
             top_torrents.append(movie) ; 
         #     # topseeds_names.append(i.text);
         #     # topseeds_links.append(i.get('href')) ;

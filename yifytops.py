86221be290d8833275274cbe082f471bad47df91
nateshmbhat <nateshmbhat1@gmail.com>
2017-11-24 08:00:02 +0530

    added torrent class which has the attributes of the torrent dictionary returned from YIFY when movies are listed and searched

--- yifytops.py
+++ yifytops.py
@@ -7,12 +7,23 @@ from requests import get
 import re 
 
 
+
 class yify():
 
     homepage = "https://www.yify-torrent.org"
 
     class torrent():
-        pass ; 
+        def __init__(self , torrents : list):
+            for i in torrents:
+                self.url = i.get('url')
+                self.hash = i.get('hash')
+                self.quality = i.get('quality')
+                self.seeds = i.get('seeds')
+                self.peers = i.get('peers')
+                self.size = i.get('size')
+                self.date_uploaded = i.get('date_uploaded')
+                
+            
 
 
     class movie():
@@ -53,11 +64,11 @@ Torrent info obtained : {}'''.format(self.name , self.page , True if hasattr(sel
             data = json.loads(resp.text) ; 
             movie = data.get('data').get("movies")[0] ;
             print(url) ;
-            return __get_movies_obj__(movie) ;
+            self.__get_movies_obj__(movie) ;  
 
         
 
-        def __get_movies_obj__(self , movie) : 
+        def __get_movies_obj__(self , movie : dict) : 
                 self.id = movie.get('id')
                 self.url = movie.get('url')
                 self.imdb_code = movie.get('imdb_code')
@@ -77,14 +88,16 @@ Torrent info obtained : {}'''.format(self.name , self.page , True if hasattr(sel
                 movie.get('small_cover_image'),
                 movie.get('medium_cover_image'),
                 movie.get('large_cover_image')]
-                self.torrents_list = movie.get('torrents') ;
-    
+
+                self.torrents = yify.torrent(movie.get('torrents'))
 
 
-    def search_movies(self , search_string = '' , quality='All' , minimum_rating = 0 , 
-        genre='' ):
+
+    def search_movies(self , search_string : str = ''  , quality: str = 'All', minimum_rating : float  = 0 , 
+        genre  : str = '') -> list:
         '''Used to search for particular movies which match the given parameters. 
-        The Search String can be a Movie Title/IMDb Code, Actor Name/IMDb Code, Director Name/IMDb Code '''
+        The Search String can be a Movie Title/IMDb Code, Actor Name/IMDb Code, Director Name/IMDb Code 
+        Returns a list of Movies each movie object having complete details about it '''
 
         self.name = re.search("^[^\(]+" , search_string).group(0).strip() ; 
         print(self.name) ; 
@@ -116,19 +129,14 @@ Torrent info obtained : {}'''.format(self.name , self.page , True if hasattr(sel
 
          
 
-        
 
-
-
-
-    def get_top_seeded_torrents(self):
+    def get_top_seeded_torrents(self : yify):
         '''Returns a list of Top Seeded Torrent's Movies which are listed in the Yify Website. 
         Each movie in the returned list contains only the 'movie name' and its corresponding 'page' attributes .
         All the rest of the details about the movie and the torrent can be obtained after calling the get_movie_info function on the movie object'''
 
         soup = BeautifulSoup(get(self.homepage , timeout=3).text , 'html.parser') ;
         topseeds = soup.find(id="topseed").find_all('a');
-
 
         top_torrents = [] 
         name_link = {} ; 
@@ -142,11 +150,10 @@ Torrent info obtained : {}'''.format(self.name , self.page , True if hasattr(sel
             movie = self.movie(name = key , page  = val) ; 
             top_torrents.append(movie) ; 
 
-        print(name_link) ; 
-
-
  
         
 if __name__=='__main__':
     obj = yify() ; 
     obj.get_top_seeded_torrents() ;
+    print(getattr(obj)) ; 
+    print(getattr(obj.torrent)) ;

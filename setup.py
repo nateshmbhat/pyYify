5be511f07f94fc1766bacf8c9382757f77f0fa4c
nateshmbhat <nateshmbhat1@gmail.com>
2017-11-25 13:31:50 +0530

    modified setup.py

--- setup.py
+++ setup.py
@@ -37,9 +37,9 @@ install_requires = ['bs4' , 'urllib3' ]
 
 setup(
     name='pyYify',
-    packages = ['Yify'] ,
-    version = 'v1.2',
-    py_modules =['os' , 're', 'json' , 'urllib' , 'time' , 'requests']
+    packages = ['pyYify'] ,
+    version = 'v1.3',
+    py_modules =['os' , 're', 'json' , 'urllib' , 'time' , 'requests'] , 
     description = '''
     This Module is used to get the Top seeded torrents at any given time and get the entire movie details and ratings . 
     Its also useful to search for any movie using different parameters and obtain their magnet link or torrent file of any prefered quality.
